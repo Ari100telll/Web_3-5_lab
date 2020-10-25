9e38a50d132cbc60cdc8e92dba95d73a15716403
Nikita Horb <masternik007@gmail.com>
2020-10-25 20:01:23 +0200

    Lab_4-5

--- Scripts/counter_script.js
+++ Scripts/counter_script.js
@@ -3,7 +3,7 @@ export function countTotalPrice(data) {
     const contents = document.getElementsByClassName("content_block");
     for (let i = 0; i < data.length; i++) {
         if (contents[i].style.display == "flex") {
-            count += parseInt(data[i]["price"]);
+            count += parseInt(data[i]["price_in_uah"]);
         }
     }
     const total = document.getElementsByClassName('total_text');
